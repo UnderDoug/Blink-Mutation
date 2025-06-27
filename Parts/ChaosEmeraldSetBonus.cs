38293219455c83f5421238a9ebaee48711d189cf
UnderDoug <douglas.marken@gmail.com>
2025-06-27 23:23:31 +1000

    Adjustments to the chaos emeralds.
    Added a WorldBuilderExtension to disperse the chaos emeralds. They're pretty busted, so they're fairly deep down.
    
    Attempted to add a "set bonus" for the chaos emeralds but it's not entirely working.
    
    Trolls should know the locations of the chaos emeralds. Make of that what you will.

---
+++ Parts/ChaosEmeraldSetBonus.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+using XRL.UI;
+using XRL.World.Effects;
+using XRL.World.Parts.Skill;
+using XRL.World.Tinkering;
+
+using UD_Blink_Mutation;
+
+using static UD_Blink_Mutation.Const;
+using static UD_Blink_Mutation.Options;
+using static UD_Blink_Mutation.Utils;
+
+using Debug = UD_Blink_Mutation.Debug;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class ChaosEmeraldSetBonus : IActivePart
+    {
+        public bool BonusActive;
+
+        public ChaosEmeraldSetBonus()
+        {
+            BonusActive = false;
+            WorksOnEquipper = true;
+            WorksOnWearer = true;
+            IsEMPSensitive = false;
+            IsPowerLoadSensitive = false;
+            IsPowerSwitchSensitive = false;
+            IsBootSensitive = false;
+            ChargeUse = 0;
+        }
+
+        public static bool CheckEquipped(GameObject Wielder)
+        {
+            List<GameObject> chaosEmeraldList = Event.NewGameObjectList(Wielder.GetEquippedObjects());
+            chaosEmeraldList.RemoveAll(GO => !GO.InheritsFrom("BaseChaosEmerald"));
+            return !chaosEmeraldList.IsNullOrEmpty() && chaosEmeraldList.Count == 7;
+        }
+
+        public bool GrantBonus(GameObject Wielder)
+        {
+            bool granted = false;
+            if(!BonusActive)
+            {
+                switch (ParentObject.Blueprint)
+                {
+                    case "Green Chaos Emerald":
+                        StatShifter.SetStatShift(Wielder, "Agility", 10);
+                        break;
+                    case "Red Chaos Emerald":
+                        StatShifter.SetStatShift(Wielder, "Strength", 10);
+                        break;
+                    case "Blue Chaos Emerald":
+                        StatShifter.SetStatShift(Wielder, "Move Speed", 200);
+                        break;
+                    case "Yellow Chaos Emerald":
+                        StatShifter.SetStatShift(Wielder, "Toughness", 10);
+                        break;
+                    case "Cyan Chaos Emerald":
+                        Wielder.ModIntProperty("ChargeRangeModifier", 10);
+                        break;
+                    case "Pink Chaos Emerald":
+                        Wielder.RegisterEvent(this, ApplyEffectEvent.ID, EventOrder.EXTREMELY_EARLY, true);
+                        break;
+                    case "Orange Chaos Emerald":
+                        GlimmerAlteration glimmerAlteration = ParentObject.RequirePart<GlimmerAlteration>();
+                        glimmerAlteration.Amount = 250;
+                        glimmerAlteration.ChargeUse = 0;
+                        glimmerAlteration.IsEMPSensitive = false;
+                        glimmerAlteration.IsPowerLoadSensitive = false;
+                        glimmerAlteration.IsTechScannable = false;
+                        break;
+                }
+                granted = StatShifter.SetStatShift(Wielder, "Speed", 15);
+            }
+            return granted;
+        }
+
+        public bool UnGrantBonus(GameObject Wielder)
+        {
+            bool unGranted = false;
+            if (BonusActive)
+            {
+                switch (ParentObject.Blueprint)
+                {
+                    case "Green Chaos Emerald":
+                        StatShifter.RemoveStatShift(Wielder, "Agility");
+                        break;
+                    case "Red Chaos Emerald":
+                        StatShifter.RemoveStatShift(Wielder, "Strength");
+                        break;
+                    case "Blue Chaos Emerald":
+                        StatShifter.RemoveStatShift(Wielder, "Move Speed");
+                        break;
+                    case "Yellow Chaos Emerald":
+                        StatShifter.RemoveStatShift(Wielder, "Toughness");
+                        break;
+                    case "Cyan Chaos Emerald":
+                        Wielder.ModIntProperty("ChargeRangeModifier", 10);
+                        break;
+                    case "Pink Chaos Emerald":
+                        Wielder.UnregisterEvent(this, ApplyEffectEvent.ID);
+                        break;
+                    case "Orange Chaos Emerald":
+                        if (ParentObject.TryGetPart(out GlimmerAlteration glimmerAlteration))
+                        {
+                            ParentObject.RemovePart(glimmerAlteration);
+                        }
+                        break;
+                }
+                StatShifter.RemoveStatShift(Wielder, "Speed");
+                unGranted = true;
+            }
+            return unGranted;
+        }
+
+        public override void Register(GameObject Object, IEventRegistrar Registrar)
+        {
+            Registrar.Register(nameof(ChaosEmeraldSetBonus));
+            base.Register(Object, Registrar);
+        }
+        public override bool WantEvent(int ID, int Cascade)
+        {
+            return base.WantEvent(ID, Cascade)
+                || ID == EquippedEvent.ID
+                || ID == UnequippedEvent.ID
+                || ID == ApplyEffectEvent.ID;
+        }
+        public override bool HandleEvent(EquippedEvent E)
+        {
+            ParentObject.FireEvent(nameof(ChaosEmeraldSetBonus));
+            return base.HandleEvent(E);
+        }
+        public override bool HandleEvent(UnequippedEvent E)
+        {
+            ParentObject.FireEvent(nameof(ChaosEmeraldSetBonus));
+            return base.HandleEvent(E);
+        }
+        public override bool HandleEvent(ApplyEffectEvent E)
+        {
+            if (BonusActive 
+                && ParentObject.Equipped != null 
+                && ParentObject.Equipped == E.Actor
+                && E.Name == nameof(ElectromagneticPulsed))
+            {
+                return false;
+            }
+            return base.HandleEvent(E);
+        }
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == nameof(ChaosEmeraldSetBonus))
+            {
+                if (ParentObject.Equipped != null)
+                {
+                    if (CheckEquipped(ParentObject.Equipped))
+                    {
+                        if (GrantBonus(ParentObject.Equipped))
+                        {
+                            BonusActive = true;
+                        }
+                    }
+                    else
+                    {
+                        if (UnGrantBonus(ParentObject.Equipped))
+                        {
+                            BonusActive = false;
+                        }
+                    }
+                }
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
---
+++ UD_ChaosEmeraldDispersalWorldBuilder.cs
@@ -0,0 +1,148 @@
+﻿using Genkit;
+using System.Collections.Generic;
+
+using XRL;
+using XRL.Rules;
+using XRL.UI;
+using XRL.Wish;
+using XRL.World;
+using XRL.World.Parts;
+using XRL.World.ZoneBuilders;
+
+using UD_Blink_Mutation;
+
+using static UD_Blink_Mutation.Const;
+using static UD_Blink_Mutation.Options;
+using static UD_Blink_Mutation.Utils;
+using XRL.Language;
+
+namespace XRL.World.WorldBuilders
+{
+    [HasWishCommand]
+    [JoppaWorldBuilderExtension]
+    public class UD_ChaosEmeraldDispersalWorldBuilder : IJoppaWorldBuilderExtension
+    {
+        private static bool doDebug => true;
+        private static bool getDoDebug(object what = null)
+        {
+            List<object> doList = new()
+            {
+                'V',    // Vomit
+            };
+            List<object> dontList = new()
+            {
+                'X',    // Trace
+            };
+
+            if (what != null && doList.Contains(what))
+                return true;
+
+            if (what != null && dontList.Contains(what))
+                return false;
+
+            return doDebug;
+        }
+
+        public JoppaWorldBuilder Builder;
+
+        public static Dictionary<string, string> ChaosEmeraldLocations = new()
+        {
+            { "Green", null },
+            { "Red", null },
+            { "Blue", null },
+            { "Yellow", null },
+            { "Cyan", null },
+            { "Pink", null },
+            { "Orange", null },
+        };
+
+        public override void OnAfterBuild(JoppaWorldBuilder Builder)
+        {
+            Debug.Header(4, $"{nameof(UD_ChaosEmeraldDispersalWorldBuilder)}", $"{nameof(OnAfterBuild)}", Toggle: doDebug);
+            MetricsManager.rngCheckpoint("chaosemeralds");
+            this.Builder = Builder;
+            Builder.BuildStep("Hiding Chaos Emeralds", HideChaosEmeralds);
+            Debug.Footer(4, $"{nameof(UD_ChaosEmeraldDispersalWorldBuilder)}", $"{nameof(OnAfterBuild)}", Toggle: doDebug);
+        }
+
+        public void HideChaosEmeralds(string WorldID)
+        {
+            int indent = Debug.LastIndent;
+            Debug.Entry(4,
+                $"* {nameof(UD_ChaosEmeraldDispersalWorldBuilder)}."
+                + $"{nameof(HideChaosEmeralds)}("
+                + $"{nameof(WorldID)}: {WorldID})",
+                Indent: indent + 1, Toggle: doDebug);
+
+            if (WorldID != "JoppaWorld")
+            {
+                Debug.LastIndent = indent;
+                return;
+            }
+            WorldCreationProgress.StepProgress("Hiding Chaos Emeralds...");
+
+            Dictionary<string, GameObject> chaosEmeralds = new()
+            {
+                { "Green", GameObject.Create("Green Chaos Emerald") },
+                { "Red", GameObject.Create("Red Chaos Emerald") },
+                { "Blue", GameObject.Create("Blue Chaos Emerald") },
+                { "Yellow", GameObject.Create("Yellow Chaos Emerald") },
+                { "Cyan", GameObject.Create("Cyan Chaos Emerald") },
+                { "Pink", GameObject.Create("Pink Chaos Emerald") },
+                { "Orange", GameObject.Create("Orange Chaos Emerald") },
+            };
+
+            Location2D parasang;
+            string zoneID;
+            string emeraldName;
+            string emeraldSecret;
+            foreach ((string color, GameObject emeraldObject) in  chaosEmeralds)
+            {
+                Debug.LoopItem(4, $"{nameof(color)}", $"{color} {emeraldObject.DebugName}",
+                    Indent: indent + 2, Toggle: doDebug);
+
+                emeraldName = $"the {color} {emeraldObject?.ShortDisplayNameStripped}";
+                emeraldSecret = $"${emeraldObject.Blueprint}".Replace(" ", "");
+                parasang = Builder.getLocationOfTier(2, 8);
+                parasang = Location2D.Get(parasang.X * 3 + 1, parasang.Y * 3 + 1);
+                zoneID = Zone.XYToID("JoppaWorld", parasang.X, parasang.Y, Stat.Random(60, 99));
+
+                Debug.LoopItem(4, $"{nameof(zoneID)}", $"{zoneID}",
+                    Indent: indent + 3, Toggle: doDebug);
+
+                The.ZoneManager.AdjustZoneGenerationTierTo(zoneID);
+                The.ZoneManager.AddZonePostBuilder(zoneID, nameof(PlaceRelicBuilder), "Relic", The.ZoneManager.CacheObject(emeraldObject));
+                string secretID = Builder.AddSecret(zoneID, emeraldName, new string[1] { "artifact" }, "Artifacts", emeraldSecret);
+
+                Debug.LoopItem(4, $"{nameof(secretID)}", $"{secretID}",
+                    Indent: indent + 3, Toggle: doDebug);
+
+                if (ChaosEmeraldLocations.ContainsKey(color))
+                {
+                    ChaosEmeraldLocations[color] = zoneID;
+                }
+            }
+            
+            Debug.Entry(4,
+                $"x {nameof(UD_ChaosEmeraldDispersalWorldBuilder)}."
+                + $"{nameof(HideChaosEmeralds)}("
+                + $"{nameof(WorldID)}: {WorldID})"
+                + $" *//",
+                Indent: indent + 1, Toggle: doDebug);
+            Debug.LastIndent = indent;
+        }
+
+        [WishCommand(Command = "go chaos emerald")]
+        public static void GoChaosEmerald(string Color)
+        {
+            if (!Color.IsNullOrEmpty() && ChaosEmeraldLocations.ContainsKey(Color))
+            {
+                Zone Z = The.ZoneManager.GetZone(ChaosEmeraldLocations[Grammar.MakeTitleCase(Color)]);
+                The.Player.Physics.CurrentCell.RemoveObject(The.Player.Physics.ParentObject);
+                Z.GetEmptyCells().GetRandomElement().AddObject(The.Player);
+                The.ZoneManager.SetActiveZone(Z);
+                The.ZoneManager.ProcessGoToPartyLeader();
+            }
+        }
+    }
+}
